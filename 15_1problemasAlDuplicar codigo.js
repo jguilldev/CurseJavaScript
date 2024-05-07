51a5c92f8e4aaf2ead8dabcc53c376b8162205fb
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-05-07 02:05:17 +0000

    send chandes in funciones y enlaces lexicos, funciones constructoras, and problemas al duplicar codigo

---
+++ 15_1problemasAlDuplicar codigo.js
@@ -0,0 +1,8 @@
+// Duplicar codigo implica
+
+// + dificil mantenimiento
+// + Aumento de la complejidad
+// + Mayor probabilidad de errores
+// + Dificultad de escalar
+// + Tiempo y recursos 
+// + Violacion de principios de diseño (DRY) Don't repeat yourself
---
+++ 16funcionesConstructoras.js
@@ -0,0 +1,23 @@
+function Rocket(nombre){  //los nombres de las funciones constructoras van en mayuscula
+    ///Se puede agregar otro parametro, "mensajePropio" y pasarlo como argumento en la linea 5
+    this.name= nombre 
+    this.launchMessage = function(){
+        console.log('Start launch ') ///"mensajePropio" aqui
+         }
+}
+
+const falcon9Rocket = new Rocket ('falcon9') //La creacion de un nuevo objeto se hace con new 
+///Aqui podriamos usar el mensajepropio pasandolo como segundo argumento linea 9
+const falconHeavyRocket = new Rocket ('falcon Heavy')
+///Aqui podriamos usar el mensajepropio pasandolo como segundo argumento linea 11
+console.log(falcon9Rocket.name)
+console.log(falcon9Rocket.launchMessage()) // no olvidar que launchMessage es una funcion y necesita()
+//  para ejecutarse
+
+
+// con arrow function no se puede usar "new" para crear nuevos objetos
+// pero si se pueden crear objetos
+
+// OBJETOS CON ARROW FUNCTION
+
+const objetosConArrowFunction =()=>
