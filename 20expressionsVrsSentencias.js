2ac322c94c294a416237a490114728b464a1914f
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-05-08 01:32:45 +0000

    Send expressions vrs sentencias and inicio proyecto biografias con JS

---
+++ 20expressionsVrsSentencias.js
@@ -0,0 +1,21 @@
+// 1.expresiones
+//Son pequeños codigos que dan un valor
+
+'hola' //produce hola
+2+3//produce 5
+6//produce 6
+
+3*(2+1)// toda es una expression
+3 //es una expresion
+2+1 //es una expresion
+2 //es una expresion
+1 //es una expresion
+
+// 2.sentencias o declaracion
+// Un fragmento que le dice al codigo que tiene que hacer 
+
+const suma =
+
+function suma1 (){
+    console.log('algo')
+}  //esta es una expresion de funcion 
---
+++ 21proyectoBiografiasConJS.js
@@ -0,0 +1,1 @@
+// inicio de clase 31 
