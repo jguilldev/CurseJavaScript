c3d2cd3bbaf1c9a258ae4ef92730a23eb66d2bd6
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-05-30 02:51:17 +0000

    send 36 shift and unshift

---
+++ 36shiftAndUnshift.js
@@ -0,0 +1,18 @@
+//el metodo Shift Elimina el primer valor en el array
+
+const numeros =[1,2,3,4,5]
+
+console.log(`${numeros}`)
+numeros.shift() //elimina el primer numero
+console.log(`${numeros}`)
+
+const electronicos =["pinza","ociloscopio","Analizador de baterias","Analizador de red","Multimetro"]
+
+console.log(`${electronicos}`)
+electronicos.unshift("Pirometro") //agrega al principio del arraY
+console.log(`${electronicos}`) //retorna los elementos del array
+
+//voy en el minuto 4:16
+
+
+
