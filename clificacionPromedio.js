3216c9cbded037ebee54ef7b862c4c52ea323be2
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-06-03 13:28:04 +0000

    send calificacioPromedi sin terminar

---
+++ clificacionPromedio.js
@@ -0,0 +1,7 @@
+// creaar un programa que tome un array de calificaciones como entrada
+// y calcular el promedio solo por calificaciones aprobadas
+// las calificaciones aprobadas deben ser mayores o iguales a 70
+
+const grades = [10,20,34,56,78,89,88,78,99,100]
+
+//clase 55 minuto 1:47
