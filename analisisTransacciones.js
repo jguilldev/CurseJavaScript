8907d95bb447cf80a2414ecd633563d8b637ae8a
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-06-08 01:11:26 +0000

    send changes analisisTransacciones

--- analisisTransacciones.js
+++ analisisTransacciones.js
@@ -18,17 +18,18 @@
 // Recuerda adaptar y combinar estas operaciones según sea necesario.
 
 const transacciones = [
-    { id: 1, descripcion: "compra de supermercado", monto: -50 },
-    { id: 2, descripcion: "venta de bicicleta", monto: 150 },
-    { id: 3, descripcion: "compra de libros", monto: -30 },
-    { id: 4, descripcion: "salario", monto: 2000 },
-    { id: 5, descripcion: "venta de muebles", monto: 500 },
-    { id: 6, descripcion: "compra de ropa", monto: -100 },
-    { id: 7, descripcion: "pago de servicios", monto: -200 },
-    { id: 8, descripcion: "freelance", monto: 300 },
-    { id: 9, descripcion: "compra de electrónica", monto: -150 },
-    { id: 10, descripcion: "regalo de cumpleaños", monto: 100 }
+    { id: 0, descripcion: "compra de supermercado", monto: -50 },
+    { id: 1, descripcion: "venta de bicicleta", monto: 150 },
+    { id: 2, descripcion: "compra de libros", monto: -30 },
+    { id: 3, descripcion: "salario", monto: 2000 },
+    { id: 4, descripcion: "venta de muebles", monto: 500 },
+    { id: 5, descripcion: "compra de ropa", monto: -100 },
+    { id: 6, descripcion: "pago de servicios", monto: -200 },
+    { id: 7, descripcion: "freelance", monto: 300 },
+    { id: 8, descripcion: "compra de electrónica", monto: -150 },
+    { id: 9, descripcion: "regalo de cumpleaños", monto: 100 }
 ];
+
 
 // saldo total de todas las transacciones
 const calculobalanceTotal = transacciones.reduce((acc,transaccion)=>acc + transaccion.monto, 0)
@@ -44,3 +45,24 @@ console.log("la maxima transaccion es:" , mayorTransaccion)
 //mostrar las transacciones de compra montos negativos mediante el metodo find
 const transaccionescompra = transacciones.filter(transaccion=>transaccion.monto<0)
 console.log(`Las transacciones por compras fueron:`,transaccionescompra)
+
+// usa el metodo find para encontrar una transaccion por su descripcion ("compra de electrónica")
+
+const encontrarTransaccion = transacciones.find(transaccion=>transaccion.descripcion==="compra de electrónica")
+console.log("la transaccion buscada por descripcion fue:", encontrarTransaccion)
+
+// encuentra el indice de una transaccion por monto usando findIndex (monto:100)
+const encontrarPorMonto = transacciones.findIndex(transaccion=>transaccion.monto === 100)
+console.log("El indice de la transaccion a la cual corresponde el monto es:",encontrarPorMonto)
+
+// actualizar las descripciones de una transaccion usando forEach 
+
+transacciones.forEach(transaccion => {
+    if (transacciones.monto < 0) {
+        transaccion.descripcion = ` gastos: ${transaccion.descripcion}`
+    } else{
+        transaccion.descripcion= `ganancias: ${transaccion.descripcion}`
+    }
+})
+
+console.log('transacciones actualizadas:',transacciones)
