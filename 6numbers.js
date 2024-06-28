0d0ba11922352d5cf9af2862ccde77e15ace80b1
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-06-28 01:46:33 +0000

    modification on 6numbers

--- 6numbers.js
+++ 6numbers.js
@@ -1,58 +1,58 @@
-// numbers es un tipo de dato primitivo
+// // numbers es un tipo de dato primitivo
 
-//numeros enteros y decimales
-const entero =9
-const decimal =0.23
-console.log(typeof entero, typeof decimal)
+// //numeros enteros y decimales
+// const entero =9
+// const decimal =0.23
+// console.log(typeof entero, typeof decimal)
 
-// notacion cientifica
-const notCientifica = 10e3
-console.log(notCientifica)
-console.log(typeof notCientifica)
+// // notacion cientifica
+// const notCientifica = 10e3
+// console.log(notCientifica)
+// console.log(typeof notCientifica)
 
-// infinito y NaN
-const infinito=Infinity
-const notAsNumber=NaN
-console.log(infinito)
-console.log(notAsNumber)
+// // infinito y NaN
+// const infinito=Infinity
+// const notAsNumber=NaN
+// console.log(infinito)
+// console.log(notAsNumber)
 
-// suma, resta multiplicacion y division
-const suma= 3+4
-const resta= 3-4
-const multiplicacion= 3*4
-const division= 4/2
+// // suma, resta multiplicacion y division
+// const suma= 3+4
+// const resta= 3-4
+// const multiplicacion= 3*4
+// const division= 4/2
 
-console.log(suma)
-console.log(resta)
-console.log(multiplicacion)
-console.log(division)
+// console.log(suma)
+// console.log(resta)
+// console.log(multiplicacion)
+// console.log(division)
 
-//modulo y exponenciacion
-const modulo =15/8
-const exponenciacion = 2**3
+// //modulo y exponenciacion
+// const modulo =15/8
+// const exponenciacion = 2**3
 
-console.log(modulo)
-console.log(exponenciacion)
+// console.log(modulo)
+// console.log(exponenciacion)
 
-// precicion 
+// // precicion 
 
-const resultado =0.3 +0.4
-console.log(resultado)
-console.log(resultado.toFixed(2))// el atributo to fixed indica la cantidad de decimales (como parametro)
+// const resultado =0.3 +0.4
+// console.log(resultado)
+// console.log(resultado.toFixed(2))// el atributo to fixed indica la cantidad de decimales (como parametro)
 
-// un = es el operador de asignacion
-// dos == es el operador de comparacion simple (no compara el tipo de dato)
-// tres === es el operador de comparacion compleja (compara el tipo de dato)
+// // un = es el operador de asignacion
+// // dos == es el operador de comparacion simple (no compara el tipo de dato)
+// // tres === es el operador de comparacion compleja (compara el tipo de dato)
 
-//Operaciones con math
+// //Operaciones con math
 
-const RaizCuadrada = Math.sqrt(16)
-const valorAbsoluto = Math.abs(-10)
-const valorRandom = Math.random()
-console.log(RaizCuadrada)
-console.log(valorAbsoluto)
-console.log(valorRandom)
-console.log(valorRandom.toFixed(3))
+// const RaizCuadrada = Math.sqrt(16)
+// const valorAbsoluto = Math.abs(-10)
+// const valorRandom = Math.random()
+// console.log(RaizCuadrada)
+// console.log(valorAbsoluto)
+// console.log(valorRandom)
+// console.log(valorRandom.toFixed(3))
 
 
 
